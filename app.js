22bff4bc014523a9544504b2671fc2b2d13bdc02
ItsHsn <47700761+HsnRao0335@users.noreply.github.com>
2020-06-19 01:48:19 +0500

    completed chapter 25-30

--- app.js
+++ app.js
@@ -1,131 +1,193 @@
 // CHAPTER 21-25
 // TASK#1
 
-// var firstName = prompt("Hello What's your first name?");
-// var lastName = prompt("Hello What's your last name?");
-// alert("Welcome " + firstName + " " + lastName);
+var firstName = prompt("Hello What's your first name?");
+var lastName = prompt("Hello What's your last name?");
+alert("Welcome " + firstName + " " + lastName);
 
-// // TASK#2
+// TASK#2
 
-// var mobile = prompt("Your favourite phone");
-// document.write(
-//   "Your favourite phone is: " +
-//     mobile +
-//     "<br/>" +
-//     "Length of string: " +
-//     mobile.length
-// );
+var mobile = prompt("Your favourite phone");
+document.write(
+  "Your favourite phone is: " +
+    mobile +
+    "<br/>" +
+    "Length of string: " +
+    mobile.length
+);
 
 // TASK#3
 
-// var country = "Pakistan";
-// alert(country + "\n index of 'n' is:  " + country.indexOf("n"));
+var country = "Pakistan";
+alert(country + "\n index of 'n' is:  " + country.indexOf("n"));
 
-// // TASK#4
-// var hello = "Hello World";
-// alert(hello + "\n last index of 'l' is:  " + hello.lastIndexOf("l"));
+// TASK#4
+var hello = "Hello World";
+alert(hello + "\n last index of 'l' is:  " + hello.lastIndexOf("l"));
 
-// // TASK#5
+// TASK#5
 
-// var country = "Pakistan";
-// alert(country + "\n character at index 3 is:  " + country.charAt(3));
+var country = "Pakistan";
+alert(country + "\n character at index 3 is:  " + country.charAt(3));
 
-// // TASK#6
-// var firstName = prompt("Hello What's your first name?");
-// var lastName = prompt("Hello What's your last name?");
-// var fullName = "Welcome " + firstName.concat(lastName);
-// alert(fullName);
+// TASK#6
+var firstName = prompt("Hello What's your first name?");
+var lastName = prompt("Hello What's your last name?");
+var fullName = "Welcome " + firstName.concat(lastName);
+alert(fullName);
 
-// // TASK#7
-// var city = "Hyderabad";
+// TASK#7
+var city = "Hyderabad";
 
-// for (var i = 0; i < city.length; i++) {
-//   if (city.slice(i, i + 5) === "Hyder") {
-//     city = city.slice(0, i) + "Islam" + city.slice(i + 5);
-//     alert(city);
-//   }
-// }
+for (var i = 0; i < city.length; i++) {
+  if (city.slice(i, i + 5) === "Hyder") {
+    city = city.slice(0, i) + "Islam" + city.slice(i + 5);
+    alert(city);
+  }
+}
 
-// // TASK#8
+// TASK#8
 
-// var str = "472";
-// var num = parseInt(str);
-// console.log(str);
-// console.log(num);
+var str = "472";
+var num = parseInt(str);
+console.log(str);
+console.log(num);
 
-// // TASK#9
+// TASK#9
 
-// var food = prompt("what do you want?");
-// alert(food.toUpperCase());
+var food = prompt("what do you want?");
+alert(food.toUpperCase());
 
-// // TASK#10
-// var enter = prompt("Enter some thing");
-// alert(enter.slice(0, 1).toUpperCase() + enter.slice(1).toLowerCase());
+// TASK#10
+var enter = prompt("Enter some thing");
+alert(enter.slice(0, 1).toUpperCase() + enter.slice(1).toLowerCase());
 
-// // TASK#11
+// TASK#11
 
-// var someNo = 33.56;
-// alert(someNo.toString().replace(".", ""));
+var someNo = 33.56;
+alert(someNo.toString().replace(".", ""));
 
 // TASK#12
 
-// var userName = prompt("Enter your username");
-// for (var i = 0; i < userName.length; i++) {
-//   if (
-//     userName[i] === String.fromCharCode(33) ||
-//     userName[i] === String.fromCharCode(44) ||
-//     userName[i] === String.fromCharCode(46) ||
-//     userName[i] === String.fromCharCode(64)
-//   ) {
-//     alert("Invalid username");
-//     break;
-//   } else if (i === userName.length - 1) {
-//     alert("Welcome back!");
-//     break;
-//   }
-// }
+var userName = prompt("Enter your username");
+for (var i = 0; i < userName.length; i++) {
+  if (
+    userName[i] === String.fromCharCode(33) ||
+    userName[i] === String.fromCharCode(44) ||
+    userName[i] === String.fromCharCode(46) ||
+    userName[i] === String.fromCharCode(64)
+  ) {
+    alert("Invalid username");
+    break;
+  } else if (i === userName.length - 1) {
+    alert("Welcome back!");
+    break;
+  }
+}
 
 // TASK#13
 
-// var loginPassword = prompt("please enter your password");
-// for (var i = 0; i < loginPassword.length; i++) {
-//   if (
-//     loginPassword[i] === String.fromCharCode(48) ||
-//     loginPassword[i] === String.fromCharCode(49) ||
-//     loginPassword[i] === String.fromCharCode(50) ||
-//     loginPassword[i] === String.fromCharCode(51) ||
-//     loginPassword[i] === String.fromCharCode(52) ||
-//     loginPassword[i] === String.fromCharCode(53) ||
-//     loginPassword[i] === String.fromCharCode(54) ||
-//     loginPassword[i] === String.fromCharCode(55) ||
-//     loginPassword[i] === String.fromCharCode(56) ||
-//     loginPassword[i] === String.fromCharCode(57)
-//   ) {
-//     if (
-//       loginPassword[i].slice(i) === String.fromCharCode(48) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(49) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(50) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(52) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(53) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(54) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(55) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(56) ||
-//       loginPassword[i].slice(i) === String.fromCharCode(57)
-//     ) {
-//       alert("password cannot begin with a number");
-//       break;
-//     } else if (i === loginPassword.length - 1) {
-//       alert("Welcome");
-//     }
-//   } else if (i === loginPassword.length - 1) {
-//     alert("Your password must contain numbers");
-//     break;
-//   }
-// }
+var loginPassword = prompt("please enter your password");
+for (var i = 0; i < loginPassword.length; i++) {
+  if (
+    loginPassword[i] === String.fromCharCode(48) ||
+    loginPassword[i] === String.fromCharCode(49) ||
+    loginPassword[i] === String.fromCharCode(50) ||
+    loginPassword[i] === String.fromCharCode(51) ||
+    loginPassword[i] === String.fromCharCode(52) ||
+    loginPassword[i] === String.fromCharCode(53) ||
+    loginPassword[i] === String.fromCharCode(54) ||
+    loginPassword[i] === String.fromCharCode(55) ||
+    loginPassword[i] === String.fromCharCode(56) ||
+    loginPassword[i] === String.fromCharCode(57)
+  ) {
+    if (
+      loginPassword[i].slice(i) === String.fromCharCode(48) ||
+      loginPassword[i].slice(i) === String.fromCharCode(49) ||
+      loginPassword[i].slice(i) === String.fromCharCode(50) ||
+      loginPassword[i].slice(i) === String.fromCharCode(52) ||
+      loginPassword[i].slice(i) === String.fromCharCode(53) ||
+      loginPassword[i].slice(i) === String.fromCharCode(54) ||
+      loginPassword[i].slice(i) === String.fromCharCode(55) ||
+      loginPassword[i].slice(i) === String.fromCharCode(56) ||
+      loginPassword[i].slice(i) === String.fromCharCode(57)
+    ) {
+      alert("password cannot begin with a number");
+      break;
+    } else if (i === loginPassword.length - 1) {
+      alert("Welcome");
+    }
+  } else if (i === loginPassword.length - 1) {
+    alert("Your password must contain numbers");
+    break;
+  }
+}
 
 // TASK#14
 
 var university = "University of karachi";
 for (var i = 0; i < university.length; i++) {
   document.write(university[i] + "<br/>");
+}
+
+// CHAPTER 25-30
+// TASK#1
+
+var decimal = prompt("Enter a decimal number like 1.232");
+
+document.write(
+  "Number: " +
+    decimal +
+    "<br> Round off value: " +
+    Math.round(decimal) +
+    "<br> Floor value: " +
+    Math.floor(decimal) +
+    "<br> Ciel value: " +
+    Math.ceil(decimal)
+);
+
+// TASK#2
+var decimalNegative = prompt("Enter a decimal number like 1.232");
+
+document.write(
+  "<br>Number: " +
+    decimalNegative +
+    "<br> Round off value: " +
+    Math.round(decimalNegative) +
+    "<br> Floor value: " +
+    Math.floor(decimalNegative) +
+    "<br> Ciel value: " +
+    Math.ceil(decimalNegative)
+);
+
+// TASK#3
+
+var absoluteNumber = prompt("Enter a value");
+alert(Math.abs(absoluteNumber));
+
+// TASK#14
+
+var dice = Math.random() * 7;
+
+alert("Random Dice Value: " + Math.round(dice));
+
+// TASK#15
+
+var randomNumber = Math.random() * 100;
+
+alert("Random number between 1 and 100 is: " + Math.round(randomNumber));
+
+// TASK#16
+
+var userWeight = parseInt(prompt("What's your wieght?"));
+alert(userWeight);
+
+// TASK#17
+
+var secretNum = Math.random() * 10;
+var askSecret = +prompt("Guess the secret number betwenn 1 to 10");
+if (Math.round(secretNum) === askSecret) {
+  alert("You Win");
+} else {
+  alert("try again");
 }
